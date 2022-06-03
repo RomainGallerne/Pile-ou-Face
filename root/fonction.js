d7c7b362768d251bf383142b738a6c30b5e643e9
RomainGallerne <romain.gallerne08@gmail.com>
2022-06-03 14:53:52 +0200

    update

--- root/fonction.js
+++ root/fonction.js
@@ -2,51 +2,50 @@ var pile = 0;
 var face = 0;
 
 function Tirage(){
-    //let html = '';
+    document.getElementById('lancer').disabled = true;
+    console.log(document.getElementById('lancer').disabled);
 
     if((Date.now()) % 2){
-      //html += 'Pile';
+      console.log('Pile');
       pile += 1;
-      Rotation("Pile.png","Face.png");
+      Rotation("https://raw.githubusercontent.com/RomainGallerne/Pile-ou-Face/main/images/Pile.png","https://raw.githubusercontent.com/RomainGallerne/Pile-ou-Face/main/images/Face.png");
     }
     else {
-      //html += 'Face';
+      console.log('Face');
       face += 1;
-      Rotation("Face.png","Pile.png");
+      Rotation("https://raw.githubusercontent.com/RomainGallerne/Pile-ou-Face/main/images/Face.png","https://raw.githubusercontent.com/RomainGallerne/Pile-ou-Face/main/images/Pile.png");
     }
-    //document.getElementById("main").innerHTML = "   ";
-    //setTimeout("TirageAffiche('"+html+"')", 100);
-}
-
-function TirageAffiche(html){
-    document.getElementById('main').innerHTML = html;
 }
 
 function Rotation(vraiIm,fausseIm){
-    let max = 4;
+    let max = 8;
     let i = 0;
-    console.log(vraiIm);
-    console.log(document.getElementById("idImg").src);
     if(vraiIm != document.getElementById("idImg").src){max+=1;}
     setTimeout(function(){
+        document.getElementById("main").setAttribute("class","top");
         let intervalId = setInterval(function(){
-            console.log(i);
             document.getElementById("idImg").setAttribute("class","rotated");
             if(i>=max){
                 clearInterval(intervalId);
                 document.getElementById("idImg").src = vraiIm;
                 document.getElementById("idImg").setAttribute("class","piece");
             }
-            else {i+=1;}
-            if(fausseIm == document.getElementById("idImg").src){
-                document.getElementById("idImg").src = vraiIm;
-            } else {
-                document.getElementById("idImg").src = fausseIm;
-            }
-            document.getElementById("idImg").clientHeight = document.getElementById("idImg").clientWidth;
-            /*setTimeout(function(){
-                document.getElementById("idImg").setAttribute("class","piece");
-            }, 100);*/ 
-        }, 500);
+            else {
+                if(i>=max-2){
+                    document.getElementById("main").setAttribute("class","bot");
+                }
+                i+=1;
+                document.getElementById("idImg").clientHeight = document.getElementById("idImg").clientWidth;
+                setTimeout(function(){
+                    document.getElementById("idImg").setAttribute("class","piece");
+                    if(fausseIm == document.getElementById("idImg").src){
+                        document.getElementById("idImg").src = vraiIm;
+                    } else {
+                        document.getElementById("idImg").src = fausseIm;
+                    }
+                }, 200);
+            } 
+        }, 400);
     }, 100);
+    document.getElementById("lancer").disabled = false;
 }
